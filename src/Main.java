2364226e2aeb20b1f53c44b202fa48fc9eef85af
Shushan <shushankhachatryan005@gmail.com>
2022-12-26 01:46:25 +0400

    Add new java file

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int c;
+        int a = 5;
+        int b = 7;
+        c = 5 * 5 + 7 * 7;
+        System.out.println("c = 5 * 5 + 7 * 7 = " + c);
+        //--------------------------------------------------//
+        a = 6;
+        b = 8;
+        c = 4;
+        int S;
+        S = (a + b) / 2 * c;
+        System.out.println("S = (a+b)/2 * c = " + S);
+        //--------------------------------------------------//
+        int m = 367;
+        System.out.println(m / 100);
+        System.out.println((m % 100 - m % 10) / 10);
+        System.out.println(m % 10);
+        //-------------------------------------------------//
+    }
+}
